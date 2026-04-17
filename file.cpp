c19927c9cd47bc2137f83c933f424e027747150b
sigma <r2d2jonny@gmail.com>
2026-04-17 23:39:45 +0000

    deleted:    file.cpp 	new file:   main.c

--- file.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main () {
-cout << "this is a repository";
-  return 0;
-}
---
+++ main.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include <string.h>
+int main(void){
+    printf("hi")// over time i will change this alot every day or a few times a day who knows :shrug: 
+}

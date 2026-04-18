bc9d4d6bc65ec22963e8d314f9befdb0a456ce0b
sigma <r2d2jonny@gmail.com>
2026-04-18 02:24:38 +0000

    modified:   main.c

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 int main(void){
     printf("hi");// over time i will change this alot every day or a few times a day who knows :shrug: 
+    int braincells = 5;
+    bool alive = true;
+    if alive == false {
+        printf("rest in peace");
+        wait(1000);
+        return 0;
+    }
 }

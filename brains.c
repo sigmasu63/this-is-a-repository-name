4b05d24e3e3b1809f0c6d1cc3f0f9b1162eca545
sigma <r2d2jonny@gmail.com>
2026-04-22 02:03:33 -0400

    removed redundant logic
    Removed braincells variable and related conditions.

--- brains.c
+++ brains.c
@@ -12,27 +12,12 @@
 int main(void) {
     printf("hi");
 
-    int braincells = 5;
     bool alive = true;
 
     if (alive == false) {
         printf("rest in peace");
         SLEEP(1000);
-        return 1;
+        return 0;
     }
-   else if (braincells == 0) {
-        printf("yeah u dead");
-        SLEEP(1000);
-        return 1;
-    }
-    else if (braincells < 10) {
-        printf("you are very dumb but alive");
-    }
-    else if (braincells > 30) {
-        printf("kind of smart");
-    }
-    else if (braincells > 20) {
-        printf("middle land smarts");
-    }
-    return 0;
+
 }
